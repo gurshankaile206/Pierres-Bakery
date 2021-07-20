b00d7d35913d4a09a6d5981fc0abe94c8b79ec8f
Gurshan Kaile <gurshankaile206@gmail.com>
2021-07-20 01:06:07 -0700

    add user greetings in console

--- PierresBakery/Models/Bakery.cs
+++ PierresBakery/Models/Bakery.cs
@@ -4,16 +4,21 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
+   
     public int BreadCost()
     {
       
     }
   }
+
   public class Bake
   {
     static void Main(string[] args)
     {
-
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("We're offering the following deals today!");
+      Console.WriteLine("Bread: buy 2, get 1 free. A single loaf costs $5");
+      Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5");
     }
   }
 }
