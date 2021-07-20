7255f3afb5284eca1dc7e1d2efff1aa8a2c554dd
Gurshan Kaile <gurshankaile206@gmail.com>
2021-07-20 00:58:50 -0700

    add initial project files

---
+++ PierresBakery.Tests/ModelTests/BakeryTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+
+namespace ProjectName.Tests
+{
+  [TestClass]
+  public class BakeTests
+  {
+    // Test methods go here
+  }
+}
---
+++ PierresBakery/Models/Bakery.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PierresBakery.Models
+{
+  public class Bake
+  {
+    static void Main(string[] args)
+    {
+      
+    }
+  }
+}
