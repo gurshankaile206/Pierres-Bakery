65c97f0980646992182374b871c04f86820681da
Gurshan Kaile <gurshankaile206@gmail.com>
2021-07-25 21:10:34 -0700

    add Bread instance passing test

--- PierresBakery.Tests/ModelTests/BakeryTests.cs
+++ PierresBakery.Tests/ModelTests/BakeryTests.cs
@@ -1,11 +1,75 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PierresBakery.Models;
+using System;
+using System.IO;
 
-namespace ProjectName.Tests
+namespace PierresBakery.Tests
 {
   [TestClass]
-  public class BreadTests
+  public class BakeryTests
   {
-    
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+
+    }
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // [TestClass]
+  // public class BreadTests
+  // {
+  //   private Bread _bread;
+  //   public BreadTests()
+  //   {
+  //     _bread = new Bread();
+  //   }
+  //   [TestMethod]
+  //   public void BreadCostTest()
+  //   {
+  //       _bread.numOfBread = 3;
+  //       Assert.AreEqual(_bread.BreadCost(), 10);
+
+  //       _bread.numOfBread = 10;
+  //       Assert.AreEqual(_bread.BreadCost(), 10 *  5);
+
+  //   }
+  // }
+  // [TestClass]
+  // public class PastryTests
+  // {
+  //   private Pastry _pastry;
+  //   public PastryTests()
+  //   {
+  //     _pastry = new Pastry();
+  //   }
+  //   [TestMethod]
+  //   public void PastryCostTest()
+  //   {
+  //       _pastry.numOfPastry = 3;
+  //       Assert.AreEqual(_pastry.PastryCost(), 5); // disocunt test
+
+  //       _pastry.numOfPastry = 1;
+  //       Assert.AreEqual(_pastry.PastryCost(), 2); // regular amount test
+
+  //       _pastry.numOfPastry = 10;
+  //       Assert.AreEqual(_pastry.PastryCost(), 10*2); // 
+  //   }
+  // }
+  
 }
